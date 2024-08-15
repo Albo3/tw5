052f6d35c93b099ef8ddb6995b0dcf500bc70f95
SulekOS <48435153+Albo3@users.noreply.github.com>
2024-08-15 21:25:01 +0800

    adminbackend

---
+++ admin/config.js
@@ -0,0 +1,13 @@
+window.CMS_CONFIGURATION = {
+  CMS_MANUAL_INIT: true,
+  config: {
+    backend: {
+      name: "github",
+      repo: "albo3/tw5",
+      branch: "main",
+      base_url: "https://api.github.com",
+      auth_endpoint: "auth",
+    },
+    // ... rest of your config
+  },
+};
